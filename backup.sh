32318e9d2ce2b33cf8c2f16f689c374ca5bfc30e
LambdaXymox <lambda.xymox@gmail.com>
2020-04-30 13:10:51 -0500

    Update backup script to account for changes in dhcpcd causing rsync to go into an infinite loop.

--- backup.sh
+++ backup.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-rsync -aHAXS --delete --info=progress2 --exclude={"/dev/*","/proc/*","/sys/*","/tmp/*","/run/*","/mnt/*","/media/*","/lost+found"} "/" "/media/backup/"
+# NOTE: We include the directory /var/lib/dhcpcd/ in the exclude list because 
+# on linux systems running dhcpcd >= 9.0.0, dhcpcd mounts several system subdirectories
+# that put rsysnc in a loop that prevents it from completing.
+rsync -aHAXS --delete --info=progress2 \
+	--exclude={"/dev/*","/proc/*","/sys/*","/tmp/*","/run/*","/mnt/*","/media/*","/var/lib/dhcpcd/*","/lost+found"} \
+	"/" "/media/backup/"
